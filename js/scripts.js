843e2f635a89fba2f893af223dbc6f4935981eb7
Edgar <eedgaarlopez02@gmail.com>
2024-02-04 20:05:44 +0100

    Primera funcion hecha

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 function overlayRegister(){
     let registro = document.getElementById("register");
 
+    registro.style.position = "absolute";
+    registro.style.display = "flex";
     registro.style.visibility = "visible";
     registro.style.opacity = "1";
-    registro.style.width = "100vw"
+    registro.style.width = "100vw";
     registro.style.height = "100vh"
 }
 function overlayLogin(){
