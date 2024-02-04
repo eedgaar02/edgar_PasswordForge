7ca8cd1abe799b0d8186525fea0e39971aa55e44
Edgar <eedgaarlopez02@gmail.com>
2024-02-04 19:31:39 +0100

    Primera funcion overlay

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function overlayRegister(){
+    let registro = document.getElementById("register");
+
+    registro.style.visibility = "visible";
+    registro.style.opacity = "1";
+    registro.style.width = "100vw"
+    registro.style.height = "100vh"
+}
+function overlayLogin(){
+
+}
