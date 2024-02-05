03e37808240ba0f5bac9d6cde1b704216474a87f
Edgar <eedgaarlopez02@gmail.com>
2024-02-05 10:07:08 +0100

    Funciones de abrir y cerrar overlay acabadas

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 function overlayRegister(){
     let registro = document.getElementById("register");
-
-    registro.style.position = "absolute";
     registro.style.display = "flex";
-    registro.style.visibility = "visible";
-    registro.style.opacity = "1";
-    registro.style.width = "100vw";
-    registro.style.height = "100vh"
 }
 function overlayLogin(){
+    let registro = document.getElementById("login");
+    registro.style.display = "flex";
+}
+
+
+function hideRegister() {
+    let registro = document.getElementById("register").style.display="none";
+
+}
+function hideLogin() {
+    let registro = document.getElementById("login").style.display="none";
 
 }
