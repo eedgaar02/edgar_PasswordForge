57bbac3c3852d47e9730d624f0b626336ccc785c
Edgar <eedgaarlopez02@gmail.com>
2024-02-05 10:55:59 +0100

    Funcion validar registro

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,47 @@
+var validado = false;
+
 function overlayRegister(){
     let registro = document.getElementById("register");
     registro.style.display = "flex";
 }
 function overlayLogin(){
-    let registro = document.getElementById("login");
-    registro.style.display = "flex";
+    let login = document.getElementById("login");
+    login.style.display = "flex";
 }
-
-
 function hideRegister() {
-    let registro = document.getElementById("register").style.display="none";
+    let registro = document.getElementById("register");
+    registro.style.display="none";
 
 }
 function hideLogin() {
-    let registro = document.getElementById("login").style.display="none";
+    let login = document.getElementById("login");
+    login.style.display="none";
+
+}
+function validarRegister(){
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let usuario = document.getElementById("usuario").value;
+    let email = document.getElementById("email").value;
+    let contraseña = document.getElementById("contraseña").value;
+    let contraConf = document.getElementById("contraseñaConf").value;
+
+    let errores = document.getElementById("errores");
+
+    if(nombre === "" || apellidos === "" || usuario === "" || email === "" || contraseña === "" || contraConf === ""){
+        errores.innerHTML = "Te has dejado algun campo sin rellenar"
+    }if(contraseña != "^(?=.*[A-Z])(?=.*\d).{8,16}$"){
+        errores.innerHTML = "La contraseña debe poseer al menos una mayuscula, un numero minimo y tiene que tener una longitud entre 8 y 16 caracteres"
+    }if(contraConf != contraseña){
+        errores.innerHTML = "La confirmacion de la contraseña no coincide con la contraseña"
+    }
+    
+}
+function generarContraseña(){
+    if(validado){
+
+    }if(!validado){
+
+    }
 
 }
