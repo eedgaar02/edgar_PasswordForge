d8507b1b4dbf99ee7d7e8f3eb9ca90dcc54dfeb9
Edgar <eedgaarlopez02@gmail.com>
2024-02-08 19:12:58 +0100

    funcion validar Registro

--- php/funciones.php
+++ php/funciones.php
@@ -1,18 +1,39 @@
 <?php
     include("conexion.php");
 
-    $nombre = $_POST["nombre"];
-    $apellidos = $_POST["apellido"];
-    $usuario = $_POST["usuario"];
-    $email = $_POST["email"];
-    $contraseña = $_POST["contraseña"];
-    $contraConf = $_POST["contraseñaConf"];
-
+    
     $emailLogIn = $_POST["emailLogIn"];
     $contraseñaLogIn = $_POST["contraseñaLogIn"];
-
+    
     function validarRegisterPhp(){
+        global $connector;
+
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellido"];
+        $usuario = $_POST["usuario"];
+        $email = $_POST["email"];
+        $contraseña = $_POST["contraseña"];
+        $contraConf = $_POST["contraseñaConf"];
+
+        $query = "SELECT * FROM usuarios WHERE email='$email'";
+        $sentencia = mysqli_query($connector, $query);
+
         if(empty($nombre) || empty($apellidos) || empty($usuario) || empty($email) || empty($contraseña) || empty($contraConf)){
-            
+            return false;
+        }elseif($contraseña != "^(?=.*[A-Z])(?=.*\d).{8,16}$"){
+            return false;
+        }elseif($contraseña != $contraConf){
+            return false;
+        }elseif($sentencia->num_rows > 0){
+            return false;
+        }else{
+        $query = "INSERT INTO usuarios (nombre, email, contraseña) VALUES ('$nombre', '$email', '$contraseña')";
+        if ($connector->query($query) === TRUE) {
+            return true;
+        } else {
+            echo "Error: " . $query . "<br>" . $connector->error;
+            return false;
+        }
         }
     }
+
